d60e49f99d337e110050acbb7c4b0499f7008c00
gdplentie3 <pedrocanudinho@gmail.com>
2026-05-03 21:02:39 -0300

    criei o gta6

---
+++ projeto/gta6.lua
@@ -0,0 +1,7 @@
+titulo="gta6"
+fps_minimo="360"
+graficos="4D 16k hyper HHDD qualidade máxima, tão realista que vai dar para comer"
+gameplay="incrível, digna de GOTY, OSCAR, GRAMMLY"
+controles="WASD"
+armas="todas"
+conquistas={"Matar 5 carinhas","Roubar um carro","dar o czinho gostoso para 5 femboys","seila"}
