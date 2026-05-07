57ae58781887f8b4a1f0b9197a91d26aff2baea2
gdplentie3 <pedrocanudinho@gmail.com>
2026-05-06 21:32:26 -0300

    consertei outro bug

--- jogo/inimigos/aranha.lua
+++ jogo/inimigos/aranha.lua
@@ -2,7 +2,7 @@ local aranha=function()
     local self={}
 
     function self:mover()
-        if(proximo_do_player())atacar_player()end
+        if(proximo_do_player())then atacar_player()end
     end
 
     function self:atualizar()
